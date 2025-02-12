8e5537af6bc173b179843ce43e8a99002448a54f
galaxyol2 <deviantheproba@gmail.com>
2025-02-12 04:52:00 +0000

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -180,15 +180,15 @@ document.addEventListener("DOMContentLoaded", event => {
 // Splash texts
 const SplashT = [
   "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
+  "Best Proxy",
+  "Made by Unknown",
+  "Securly is Ass",
   "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
-  "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
+  "Securly Can't Do Shit To Me",
+  "unblocked nigga",
+  "Fuck Securly",
+  "Securly Is Breaking The Law",
+  "Bypassed",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
